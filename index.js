d7c0e51af838a3abfa8df45798f1b918e43fb2b8
snozza <andrewcsnead@gmail.com>
2017-07-09 17:39:42 +0100

    remove spaces

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use(bodyParser.text({}));
 app.options("/*", (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Accepts,Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Accepts,Content-Type,Authorization,Content-Length,X-Requested-With');
   res.send(200);
 });
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Accepts,Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Accepts,Content-Type,Authorization,Content-Length,X-Requested-With');
   next();
 });
 
